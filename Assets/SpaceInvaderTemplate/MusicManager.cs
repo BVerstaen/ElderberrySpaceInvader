0ed422b75db80c767fc6d6aaefca445062724c55
BVerstaen <bverstaen04@gmail.com>
2026-03-11 16:08:00 +0100

    [ADD] Background music

---
+++ Assets/SpaceInvaderTemplate/MusicManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private AudioSource _musicSource;
+
+    [Header("Music")]
+    [SerializeField] private AudioClip _menuMusic;
+    [SerializeField] private AudioClip _gameMusic;
+
+    private void Awake()
+    {
+        _musicSource.clip = _menuMusic;
+        _musicSource.Play();
+    }
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnStartGame += ChangeToDarkXXXMusic;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnStartGame -= ChangeToDarkXXXMusic;
+    }
+
+    private void ChangeToDarkXXXMusic()
+    {
+        _musicSource.clip = _gameMusic;
+        _musicSource.Play();
+    }
+}
