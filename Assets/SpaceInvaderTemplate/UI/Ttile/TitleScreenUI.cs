2f382974f5027b0b2c5db84636d07271665591d1
BVerstaen <bverstaen04@gmail.com>
2026-03-11 14:13:19 +0100

    [ADD] Title screen prefab

--- Assets/SpaceInvaderTemplate/UI/Ttile/TitleScreenUI.cs
+++ Assets/SpaceInvaderTemplate/UI/Ttile/TitleScreenUI.cs
@@ -9,6 +9,11 @@ public class TitleScreenUI : MonoBehaviour
     [Header("Reference")]
     [SerializeField] private TMP_Text _highscoreText;
 
+    [Header("Feedback")]
+    [SerializeField] private float _highTextSpeed;
+    [SerializeField] private float _highTextAmplitude;
+    [SerializeField] private float _highTextMinimumScale;
+
     private void Start()
     {
         if (PlayerPrefs.HasKey(PLAYER_SCORE_PREF_KEY))
@@ -26,6 +31,13 @@ public class TitleScreenUI : MonoBehaviour
         }
         else
             _highscoreText.text = "";
+    }
+
+    private void Update()
+    {
+        //highscore scale text
+        float highScoreScale = (Mathf.Sin(Time.time * _highTextSpeed) + 1 + _highTextMinimumScale) * _highTextAmplitude;
+        _highscoreText.rectTransform.localScale = new Vector3(highScoreScale, highScoreScale, highScoreScale);
     }
 
     public void StartGame()
